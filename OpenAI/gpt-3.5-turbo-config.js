251d259dede3f6004a8e8e4f8b6139cf887c7b45
velzhar <fabiancortes1999@gmail.com>
2023-11-09 00:20:57 -0300

    prueba

---
+++ OpenAI/gpt-3.5-turbo-config.js
@@ -0,0 +1,2 @@
+const tectalicOpenai = require('@tectalic/openai').default
+const openIAClient = tectalicOpenai(process.env.OPENAI_API_KEY)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Parte-2-Solemne-2
+ Prototipo de la aplicación presentada en formato web
